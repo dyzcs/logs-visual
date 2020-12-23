7d1931665e910afb17e8278418b8f77104a79783
dyzcs <deng.kinger@gmail.com>
2020-12-23 15:42:05 +0800

    Create LogsConstant.java

---
+++ logs-common/src/main/java/LogsConstant.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2020/12/23.
+ */
+public class LogsConstant {
+    // 启动日志主题
+    public static final String LOGS_STARTUP = "logs_startup";
+
+    // 事件日志主题
+    public static final String LOGS_EVENT = "logs_event";
+}
