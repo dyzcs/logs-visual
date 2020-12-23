cc437674c3c1c4493e8894099853bdcc79e28c51
dyzcs <deng.kinger@gmail.com>
2020-12-23 16:02:03 +0800

    logs-logger

---
+++ logs-logger/src/main/java/com/dyzcs/logslogger/LogsLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.dyzcs.logslogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsLoggerApplication.class, args);
+    }
+
+}
